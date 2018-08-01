d28563bfd7cdfa01e919bf11f76f78fc039557f5
fabricio zanette <fabzanette@gmail.com>
2018-08-01 19:57:12 -0300

    retreive odds of win for each team. display them not working still

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,37 @@ class PagesController < ApplicationController
         time.prepend("VASCO DA ")
       elsif time == "PRETA/SP"
         time.prepend("PONTE ")
+      elsif time == "CORRÊA/MA"
+        time.prepend("SAMPAIO ")
       end
     end
     return times_array
   end
+
+  def get_stats
+    stats_url = "http://www.mat.ufmg.br/futebol/tabela-da-proxima-rodada_seriea/"
+    unparsed_stats = HTTParty.get(stats_url)
+    parsed_stats = Nokogiri::HTML(unparsed_stats)
+    @pontos = parsed_stats.css('td').map(&:text)
+    @confrontos = @pontos.each_slice(5).to_a
+    @odds_hash = []
+    @confrontos.each do |confronto|
+      @odds_hash << {
+        confronto[0] => confronto[1],
+        confronto[4] => confronto[3]
+      }
+    end
+    @odds_hash
+  end
+
+  def display_odds
+    odds = get_stats
+
+    odds.each do |odd|
+
+    end
+  end
+
   def home
     url = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/loteca/programacao"
 
@@ -29,5 +56,6 @@ class PagesController < ApplicationController
       end
     append_names(@times)
     end
+    get_stats
   end
 end
