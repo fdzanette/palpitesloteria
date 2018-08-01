981c8709fe5fb8064195dd84094be9406f500f88
fabricio zanette <fabzanette@gmail.com>
2018-08-01 13:02:10 -0300

    try to get team full names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,18 @@ require 'nokogiri'
 require 'httparty'
 
 class PagesController < ApplicationController
+  def append_names(times_array)
+    times_array.each do |time|
+      if time == "PAULO/SP"
+        time.prepend("SÂO ")
+      elsif time == "GAMA/RJ"
+        time.prepend("VASCO DA ")
+      elsif time == "PRETA/SP"
+        time.prepend("PONTE ")
+      end
+    end
+    return times_array
+  end
   def home
     url = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/loteca/programacao"
 
@@ -10,9 +22,12 @@ class PagesController < ApplicationController
     @times = []
     @jogos = html_doc.css('tbody').text
     @jogos.split.each do |jogo|
-      if jogo.length > 3 && jogo != "Sábado" && jogo != "Domingo"
-        @times << jogo
+      if jogo.length > 2 && jogo != "Sábado" && jogo != "Domingo"
+        if jogo.include? "/"
+          @times << jogo
+        end
       end
+    append_names(@times)
     end
   end
 end
