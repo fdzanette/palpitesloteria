1c1e9b322d479e31f3c2302f951a65432d236791
fabricio zanette <fabzanette@gmail.com>
2018-08-27 18:03:43 -0300

    add color to teams td

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,20 +6,40 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-function sleep_s(secs) {
-  secs = (+new Date) + secs * 1000;
-  while ((+new Date) < secs);
-}
+
+// function sleep_s(secs) {
+//   secs = (+new Date) + secs * 1000;
+//   while ((+new Date) < secs);
+// }
 
 const button = document.getElementById('gerar');
 const refresh = document.getElementById('my-btn');
+const teams = document.querySelectorAll('.team');
 
 const placares = document.querySelectorAll('.hidden');
 
 button.addEventListener('click', (event) => {
+  let i = 0;
+  let j = 1;
   refresh.classList.add('fa-spin');
   placares.forEach((placar) => {
   placar.classList.toggle('hidden');
+
+    if (placar.text === "1 x 0" || placar.text === "2 x 0") {
+      teams[i].classList.add('winner-score');
+      teams[j].classList.add('loser-score');
+    } else if (placar.text === "0 x 1" || placar.text === "0 x 2") {
+      teams[i].classList.add('loser-score');
+      teams[j].classList.add('winner-score');
+    } else if (placar.text === "- x -") {
+      teams[i].classList.add('draw-score');
+      teams[j].classList.add('draw-score');
+    } else {
+      teams[i].classList.add('draw-score');
+      teams[j].classList.add('draw-score');
+    };
+    i += 2;
+    j += 2;
   });
 });
 
